42ec3703dcb3773bb76d5231df5d9a91f4c758c9
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-07 11:15:20 +0200

    Using useCallback in the custom hook and in App.js to prevent infinite loops and using the custom hooks requestConfig parameter in sendRequest function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import useHttp from "./hooks/useHttp";
 
 import Tasks from "./components/Tasks/Tasks";
@@ -7,29 +7,22 @@ import NewTask from "./components/NewTask/NewTask";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (taskObj) => {
+  const transformTasks = useCallback((taskObj) => {
     const loadedTasks = [];
 
     for (const taskKey in taskObj) {
       loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
     setTasks(loadedTasks);
-  };
+  }, []);
 
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: "https://tasks-with-custom-hooks-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-    },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({
+      url: "https://tasks-with-custom-hooks-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+    });
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
